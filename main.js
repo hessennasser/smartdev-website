2f7a01677c6b2786336b6d4be4916d2105ee0747
hessennasser <92742551+hessennasser@users.noreply.github.com>
2022-12-17 00:30:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const menuIcon = document.querySelector(".menu-icon");
+const navMenu = document.querySelector(".nav");
+const navLink = document.getElementsByClassName("link-header");
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+function myFunction() {
+    var element = document.getElementsByClassName("link-header");
+    menuIcon.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+// start btn-up 
+btnUp = document.querySelector('.up-btn');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 300) {
+        btnUp.classList.add("active");
+    } else {
+        btnUp.classList.remove("active");
+    }
+});
+btnUp.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+// end btn-up 
+
+// start btn-up 
+whatsBtn = document.querySelector('.whats-btn');
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 300) {
+        whatsBtn.classList.add("active");
+    } else {
+        whatsBtn.classList.remove("active");
+    }
+});
+// end btn-up 
+
+// start sticky header 
+var header = this.document.querySelector("header");
+window.addEventListener("scroll", function(){
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+// end sticky header 
