c254876e5b5c188c4a07a01e8c9fd4cf0359667e
hessennasser <92742551+hessennasser@users.noreply.github.com>
2023-01-16 00:31:00 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function myFunction() {
 btnUp = document.querySelector('.up-btn');
 
 window.addEventListener('scroll', function () {
-    if (window.scrollY > 150) {
+    if (window.scrollY > 250) {
         btnUp.classList.add("active");
     } else {
         btnUp.classList.remove("active");
@@ -33,7 +33,7 @@ btnUp.addEventListener("click", function () {
 // start btn-up 
 whatsBtn = document.querySelector('.whats-btn');
 window.addEventListener('scroll', function () {
-    if (window.scrollY > 150) {
+    if (window.scrollY > 250) {
         whatsBtn.classList.add("active");
     } else {
         whatsBtn.classList.remove("active");
@@ -43,8 +43,45 @@ window.addEventListener('scroll', function () {
 
 // start sticky header 
 var header = this.document.querySelector("header");
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 0);
-})
+});
 
 // end sticky header 
+
+// start translation 
+// const translations = {
+//     en: {
+//         home: "home",
+//         services: "services",
+//         projects: "projects",
+//         sallaStores: "salla stores",
+//         contact: "contact",
+//         englishLang: "english",
+//         arabicLang: "arabic",
+//     },
+//     ar: {
+//         home: "الصفحة الرئسية",
+//         services: "خدماتنا",
+//         projects: "مشاريعنا",
+//         sallaStores: "متاجر سلة",
+//         contact: "تواصل بنا",
+//         englishLang: "الأنجليزية",
+//         arabicLang: "العربية",
+//     },
+// };
+
+// const languageSelctor = document.querySelector("#lang");
+// languageSelctor.addEventListener("change", (event) => {
+//     setLanguage(event.target.value);
+// });
+
+// const setLanguage = (language) => {
+//     const elements = document.querySelectorAll("[data-i18n]");
+//     elements.forEach((element) => {
+//         const translationKey = element.getAttribute("data-i18n");
+//         element.textContent = translations[language][translationKey];
+//     });
+//     document.dir = language === "ar" ? "rtl" : "ltr";
+// };
+// end translation
