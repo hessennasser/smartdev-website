b02dcd3421d68317de8c399d60ddc1a1126d2c3c
Hessen Nasser <hessenaloosh@gmail.com>
2023-02-05 02:43:45 +0200

    make a filter to my project cards

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// start header icon  
 const menuIcon = document.querySelector(".menu-icon");
 const navMenu = document.querySelector(".nav");
 const navLink = document.getElementsByClassName("link-header");
@@ -5,16 +6,22 @@ menuIcon.addEventListener("click", () => {
     menuIcon.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
-
 function myFunction() {
     var element = document.getElementsByClassName("link-header");
     menuIcon.classList.toggle("active");
     navMenu.classList.toggle("active");
 }
+// end header icon  
+
+// start sticky header 
+var header = this.document.querySelector("header");
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+// end sticky header 
 
 // start btn-up 
 btnUp = document.querySelector('.up-btn');
-
 window.addEventListener('scroll', function () {
     if (window.scrollY > 250) {
         btnUp.classList.add("active");
@@ -29,8 +36,7 @@ btnUp.addEventListener("click", function () {
     });
 });
 // end btn-up 
-
-// start btn-up 
+// start whats-app btn 
 whatsBtn = document.querySelector('.whats-btn');
 window.addEventListener('scroll', function () {
     if (window.scrollY > 250) {
@@ -39,12 +45,33 @@ window.addEventListener('scroll', function () {
         whatsBtn.classList.remove("active");
     }
 });
-// end btn-up 
+// end whats-app btn 
 
-// start sticky header 
-var header = this.document.querySelector("header");
-window.addEventListener("scroll", function () {
-    header.classList.toggle("sticky", window.scrollY > 0);
+//start filter  
+let switchersLis = document.querySelectorAll(".filter-item");
+let cards = document.querySelectorAll(".work .card");
+switchersLis.forEach((li) => {
+    li.addEventListener("click", removeActive);
+    li.addEventListener("click", manageCards);
 });
-
-// end sticky header 
+// remove active class from lis and add to current 
+function removeActive() {
+    switchersLis.forEach((li) => {
+        li.classList.remove("active");
+        this.classList.add("active");
+    });
+}
+// manage cards 
+function manageCards() {
+    cards.forEach((cards) => {
+        cards.style.display = ("none");
+        cards.style.scale = ("0");
+        cards.style.opacity = ("0");
+    });
+    document.querySelectorAll(this.dataset.cat).forEach((el) => {
+        el.style.display = ("block");
+        el.style.scale = ("1");
+        el.style.opacity = ("1");
+    });
+}
+//end filter  
